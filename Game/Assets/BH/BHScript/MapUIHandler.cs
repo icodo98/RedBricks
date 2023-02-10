355725108aef097bd5c71992c9e26db14b794e11
BH1107 <123962719+BH1107@users.noreply.github.com>
2023-02-10 16:20:39 +0900

    branch unlock stage...save

--- Game/Assets/BH/BHScript/MapUIHandler.cs
+++ Game/Assets/BH/BHScript/MapUIHandler.cs
@@ -2,23 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MapUIHandler : MonoBehaviour
 {
-    public int stageNum;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Button[] Stages;
+   int stageTempMem;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+private void Start() {
+    unlockStages();
     
+}
+    private void Update() {
+        Debug.Log(stageTempMem);
+    }
 
     public void backToMain()
     {
@@ -30,8 +27,79 @@ public class MapUIHandler : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
-    public void StartStageScene()
+    public void StartStageScene(int stageNum)
     {
         SceneManager.LoadScene("Stage" + stageNum.ToString());
+    }
+
+    public void stagesUnlockReset(){
+        
+
+        for(int i = 0 ; i < Stages.Length; i++)
+        {
+            Stages[i].interactable = false;
+        }  
+        Stages[0].interactable = true;
+        stageTempMem = 0;
+    }
+    public void stageUnlocktempMem(int stageMem)
+    {
+        stageTempMem = stageMem;
+    }
+    public void unlockStages()
+    {
+        switch(stageTempMem){
+            case 1:
+                    for(int i = 0 ; i < Stages.Length; i++)
+                     {
+            Stages[i].interactable = false;
+                    }  
+                    Stages[1].interactable = true;
+                    Stages[2].interactable = true;
+                    Stages[3].interactable = true;
+                    break;
+
+            case 21:
+                     for(int i = 0 ; i < Stages.Length; i++)
+                     {
+                     Stages[i].interactable = false;
+                    }  
+                    Stages[4].interactable = true;
+    
+                    break;
+            case 22:
+
+                    for(int i = 0 ; i < Stages.Length; i++)
+                    {
+                    Stages[i].interactable = false;
+                    }  
+                        Stages[4].interactable = true;
+                        Stages[5].interactable = true;
+                    break;
+
+            case 23:
+                    for(int i = 0 ; i < Stages.Length; i++)
+                    {
+                        Stages[i].interactable = false;
+                    }  
+                    Stages[5].interactable = true;
+                    break;
+            case 31:
+                    for(int i = 0 ; i < Stages.Length; i++)
+                        {
+                        Stages[i].interactable = false;
+                    }  
+                    Stages[6].interactable = true;
+                    Stages[7].interactable = true;
+                     break;
+            case 32:
+                   for(int i = 0 ; i < Stages.Length; i++)
+                    {
+                     Stages[i].interactable = false;
+                    }  
+                     Stages[7].interactable = true;
+                     Stages[8].interactable = true;
+                    break;
+        }
     }
 }
