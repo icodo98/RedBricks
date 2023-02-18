b1847589759cd0f6ea2275bcc4e274cd8b467848
BH1107 <123962719+BH1107@users.noreply.github.com>
2023-02-18 17:33:12 +0900

    Revert "branch unlock stage...save"
    This reverts commit 355725108aef097bd5c71992c9e26db14b794e11.

--- Game/Assets/BH/BHScript/MapUIHandler.cs
+++ Game/Assets/BH/BHScript/MapUIHandler.cs
@@ -2,20 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MapUIHandler : MonoBehaviour
 {
-    public Button[] Stages;
-   int stageTempMem;
-
-private void Start() {
-    unlockStages();
-    
-}
-    private void Update() {
-        Debug.Log(stageTempMem);
+    public int stageNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
 
     public void backToMain()
     {
@@ -27,79 +30,8 @@ private void Start() {
         SceneManager.LoadScene(1);
     }
 
-    public void StartStageScene(int stageNum)
+    public void StartStageScene()
     {
         SceneManager.LoadScene("Stage" + stageNum.ToString());
-    }
-
-    public void stagesUnlockReset(){
-        
-
-        for(int i = 0 ; i < Stages.Length; i++)
-        {
-            Stages[i].interactable = false;
-        }  
-        Stages[0].interactable = true;
-        stageTempMem = 0;
-    }
-    public void stageUnlocktempMem(int stageMem)
-    {
-        stageTempMem = stageMem;
-    }
-    public void unlockStages()
-    {
-        switch(stageTempMem){
-            case 1:
-                    for(int i = 0 ; i < Stages.Length; i++)
-                     {
-            Stages[i].interactable = false;
-                    }  
-                    Stages[1].interactable = true;
-                    Stages[2].interactable = true;
-                    Stages[3].interactable = true;
-                    break;
-
-            case 21:
-                     for(int i = 0 ; i < Stages.Length; i++)
-                     {
-                     Stages[i].interactable = false;
-                    }  
-                    Stages[4].interactable = true;
-    
-                    break;
-            case 22:
-
-                    for(int i = 0 ; i < Stages.Length; i++)
-                    {
-                    Stages[i].interactable = false;
-                    }  
-                        Stages[4].interactable = true;
-                        Stages[5].interactable = true;
-                    break;
-
-            case 23:
-                    for(int i = 0 ; i < Stages.Length; i++)
-                    {
-                        Stages[i].interactable = false;
-                    }  
-                    Stages[5].interactable = true;
-                    break;
-            case 31:
-                    for(int i = 0 ; i < Stages.Length; i++)
-                        {
-                        Stages[i].interactable = false;
-                    }  
-                    Stages[6].interactable = true;
-                    Stages[7].interactable = true;
-                     break;
-            case 32:
-                   for(int i = 0 ; i < Stages.Length; i++)
-                    {
-                     Stages[i].interactable = false;
-                    }  
-                     Stages[7].interactable = true;
-                     Stages[8].interactable = true;
-                    break;
-        }
     }
 }
