31ef3e98e3af475d8b783cf173e9ae2c912b2ca8
BH1107 <123962719+BH1107@users.noreply.github.com>
2023-04-07 08:24:32 +0900

    YouWinBitSelect
    and link map to battle

--- Game/Assets/BH/BHScript/WinSceneManager.cs
+++ Game/Assets/BH/BHScript/WinSceneManager.cs
@@ -2,13 +2,69 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class WinSceneManager : MonoBehaviour
 {
-    
+    [SerializeField]
+    private Button bit1;
+    [SerializeField]
+    private Button bit2;
+    [SerializeField]
+    private Button bit3;
+    private void OnEnable()
+     {
+        switch (WinSceneBitSelect.bitselcet)
+        {
+            case Bitselcet.Bit1:
+                bit1.image.color = Color.green;
+                bit2.image.color = Color.white;
+                bit3.image.color = Color.white;
+                break;
+            case Bitselcet.Bit2:
+                bit1.image.color = Color.white;
+                bit2.image.color = Color.green;
+                bit3.image.color = Color.white;
+                break;
+            case Bitselcet.Bit3:
+                bit1.image.color = Color.white;
+                bit2.image.color = Color.white;
+                bit3.image.color = Color.green;
+                break;
+        }
+    }
+
+    public void SelectedBit(int bitselcet)
+    {
+        WinSceneBitSelect.bitselcet = (Bitselcet)bitselcet;
+        switch (WinSceneBitSelect.bitselcet)
+        {
+            case Bitselcet.Bit1:
+                bit1.image.color = Color.green;
+                bit2.image.color = Color.white;
+                bit3.image.color = Color.white;
+                break;
+            case Bitselcet.Bit2:
+                bit1.image.color = Color.white;
+                bit2.image.color = Color.green;
+                bit3.image.color = Color.white;
+                break;
+            case Bitselcet.Bit3:
+                bit1.image.color = Color.white;
+                bit2.image.color = Color.white;
+                bit3.image.color = Color.green;
+                break;
+        }
+    }
     public void TitleButton()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public void MapButton()
+    {
+        SceneManager.LoadScene(2);
     }
 
 }
