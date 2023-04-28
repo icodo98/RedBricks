994ff0e837755ae8ba0671014be04b701681f283
BH1107 <123962719+BH1107@users.noreply.github.com>
2023-04-28 10:57:54 +0900

    newReinforcementTree 中

---
+++ Game/Assets/BH/BHScript/SkillManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour
+{
+    public static SkillManager instance;
+    public Skill[] skills;
+    public SkillButton[] skillButtons;
+
+    public Skill activateSkill;
+
+    private void Awake() {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+}
