29cd9ee5764cca68689db3c71554855686e719e3
treeant <yongwoo3141@gamil.com>
2023-05-03 10:39:48 +0900

    dmg font 2

--- Game/Assets/Script/Enemytext.cs
+++ Game/Assets/Script/Enemytext.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Enemytext : MonoBehaviour
 {
     public GameObject hudDamageText;
-    public Transform hudPos;
   
     
-    public void TakeDamage(int damage)
+    public void TakeDamage(int damage,Vector3 hudPos)
     {
         GameObject hudText = Instantiate(hudDamageText);
-        hudText.transform.position = hudPos.position;
+        hudText.transform.position = hudPos;
         hudText.GetComponent<DamageText>().damage =damage;
     }
 }
