3ec2e52bffb3d658782cb29565a676ca51132e66
BH1107 <123962719+BH1107@users.noreply.github.com>
2023-06-05 11:03:18 +0900

    campfireSecenAnimation

---
+++ Game/Assets/BH/BHScript/CampfireSceneManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CampfireSceneManager : MonoBehaviour
+{
+ 
+
+  
+  public void Regeneration()
+  {
+
+  }
+
+  public void ToMap()
+  {
+    SceneManager.LoadScene(2);
+  }
+}
