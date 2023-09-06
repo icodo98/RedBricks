a3b84eb92ea7cbadf2371fe6ccf9886a6918838a
JUYOUNG LEE <rsb98759@gmail.com>
2023-09-06 11:28:54 +0900

    almost done with summon Mobs.

--- Game/Assets/Script/Boss1.cs
+++ Game/Assets/Script/Boss1.cs
@@ -11,7 +11,14 @@ public class Boss1 : MonoBehaviour, IListener
         set => _priority = value; 
     }
     public GameObject block;
-    private WaitForSeconds waitFor3Seconds = new WaitForSeconds(3);
+    private WaitForSeconds waitFor3Seconds = new (1f);
+    [SerializeField]
+    private Vector3 startPoint = new Vector3(-0.85f, -0.2f, 0f);
+
+    [SerializeField]
+    private Vector3 endPoint = new Vector3(0.8f, 1.9f, 0f);
+    private int i;
+
     public void OnEvent(myEventType eventType, Component Sender, object Param = null)
     {
         switch (eventType)
@@ -33,25 +40,62 @@ public class Boss1 : MonoBehaviour, IListener
         EventManager.Instance.AddListener(myEventType.StageClear, this);
         EventManager.Instance.AddListener(myEventType.GamePause, this);
         
-
+        StartCoroutine(summonBlocks());
     }
-    
+    /// <summary>
+    /// 잡몹을 소환하는 코루틴. 잡몹이 소환될 위치를 특정하고, 그 위치에 블럭을 소환한다.
+    /// </summary>
+    /// <returns></returns>
     IEnumerator summonBlocks()
     {
-        if(block == null) yield break;
-        Vector2 position = new Vector2();
-        if (!checkPosition(position))
+        while(true)
         {
-            Instantiate(block, position, Quaternion.identity, this.transform);
+            Vector3[] position = newPoisition();
+
+            foreach (Vector3 item in position)
+            {
+                if (!checkPosition(item)) continue;
+                GameObject MobBlock = Instantiate(block, item, Quaternion.identity, this.transform);
+                MobBlock.transform.localScale = new Vector3(0.8f, 0.2f, 1f);
+            }
+           // yield return null;
+              yield return waitFor3Seconds;
         }
-        yield return waitFor3Seconds;
+
     }
-
+    /// <summary>
+    /// 인수로 받은 위치에 collider가 존재하는지 여부르 확인하는 코드. 없다면 true, 있으면 false
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns>비었으면 true</returns>
     private bool checkPosition(Vector2 position) {
-        
 
+        Collider2D result =  Physics2D.OverlapBox(position,new Vector2(0.57f,0.14f) ,0);
+        if (result == null)
+        {
+            return true;
+        }
 
-        return false;
+        else
+        {
+            return false;
+        }
+
+    }
+    private Vector3[] newPoisition()
+    {
+        int arraySize = Random.Range(3,7);
+        Vector3[] returnArray = new Vector3[arraySize];
+        while (i < arraySize)
+        {
+            float x = Random.Range(startPoint.x, endPoint.x);
+            float y = Random.Range(startPoint.y, endPoint.y);
+            
+            returnArray[i] = new Vector3(x,y,0);
+            if (checkPosition(returnArray[i])) i++;
+
+        }
+        return returnArray;
     }
 
 }
