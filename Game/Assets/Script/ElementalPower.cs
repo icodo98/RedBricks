b39ddc38d71a9271c61a96310b07233e2d291a1a
JUYOUNG LEE <rsb98759@gmail.com>
2023-12-04 08:13:50 +0900

    still working

--- Game/Assets/Script/ElementalPower.cs
+++ Game/Assets/Script/ElementalPower.cs
@@ -39,27 +39,32 @@ public class ElementalPower : MonoBehaviour
         isCoroutineRunnig = false;
     }
 
-
+    /// <summary>
+    /// Electric elemental ability.damage multiple nearby objects.
+    /// </summary>
+    /// <param name="other"></param>
     public void ElectricPower(Collision2D other)
     {
         // Find blcoks in range
         Collider2D[] rangeBoxes = Physics2D.OverlapBoxAll(other.transform.position, ElectricSize,0);
 
-        //Todo : Exception handle. exclude ball and itself.
         foreach (Collider2D rangeBox in rangeBoxes)
         {
-            if (!rangeBox.CompareTag("Block") || rangeBox.transform == transform) continue;
-            if(rangeBox != null)
-            {
-                rangeBox.gameObject.GetComponent<Enemytext>().TakeDamage(1, rangeBox.transform.position + new Vector3(0.05f, -0.1f, 0),Color.blue);
+            if (rangeBox == null) continue;
+            if (!rangeBox.CompareTag("Block") || rangeBox.transform == transform) continue; // Exception handle. exclude ball and itself.
+            rangeBox.gameObject.GetComponent<Enemytext>().TakeDamage(1, rangeBox.transform.position + new Vector3(0.05f, -0.1f, 0), Color.blue);
 
-            }
         }
     }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(transform.position, ElectricSize);
+    }
+
+    public void WaterPower(Collision2D other)
+    {
+
     }
 }
 
