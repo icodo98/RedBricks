25d2e3a2c33c6c4890b5394abd26d8a8221f0bf3
JUYOUNG LEE <rsb98759@gmail.com>
2023-12-18 09:48:19 +0900

    added Freeze Bit

--- Game/Assets/Script/blockCollision.cs
+++ Game/Assets/Script/blockCollision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class blockCollision : MonoBehaviour
 {
+    private int FreezeStack = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name.Equals("Bottom"))
@@ -15,6 +16,17 @@ public class blockCollision : MonoBehaviour
         {
             GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerConroller>().TakeDamage(25, false);
             Destroy(this.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Ball"))
+        {
+            if(collision.gameObject.GetComponent<PlayerCollision>().FreezeBit) 
+            {
+                FreezeStack++;
+                if(FreezeStack > 5) {
+                    GetComponent<ElementalPower>().WaterPower(collision);
+                    FreezeStack = 0;
+                }
+            }
         }
 
     }
